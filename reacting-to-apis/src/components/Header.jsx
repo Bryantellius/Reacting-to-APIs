3639236a2ac3e769fab1b909ca6f26ab45ac4e63
Ben Bryant <brbryant2639@gmail.com>
2020-03-11 09:35:06 -0500

    added App, Header component to start app, rendered Ghibli logo

---
+++ reacting-to-apis/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className='header jumbotron text-center'>
+                <img src='logo.png' alt='Studio Ghibli Logo'></img>
+            </div>
+        )
+    }
+}
+
+export default Header;
