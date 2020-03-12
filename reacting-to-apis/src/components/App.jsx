55b361104ccfeac9cfbae79d9f88bb88075b7f36
Ben Bryant <brbryant2639@gmail.com>
2020-03-11 21:38:26 -0500

    updated app component to get films and append list as cards

--- reacting-to-apis/src/components/App.jsx
+++ reacting-to-apis/src/components/App.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
+// import 'isometric-fetch';
+// import 'es6-promise';
 
-function App(props) {
-    return (
-        <Header />
-    )
+class App extends Component {
+
+    state = {
+        filmsArr: []
+    }
+
+    componentDidMount() {
+        fetch('https://ghibliapi.herokuapp.com/films')
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data);
+                this.setState({
+                    filmsArr: data.map((film) => {
+                        return (
+                            <div className="card col-md-3" key={film.id}>
+                                <div className="card-body">
+                                    <h5 className="card-title">{film.title}</h5>
+                                    <p className="card-text">{film.description}</p>
+                                    <p className="card-text"><strong>{film.release_date}</strong></p>
+                                </div>
+                            </div>
+                        );
+                    })
+                })
+            })
+            .catch(err => console.log(err));
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <Header />
+                <hr></hr>
+                <div className='row text-center'>{this.state.filmsArr}</div>
+            </React.Fragment >
+        );
+    }
 }
 
 export default App;
